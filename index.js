f459acdeb9c8b04e5e853c7af358bd1657c42e63
ibrahimmohamed2411 <ibrahimmohamed2411@gmail.com>
2023-03-16 01:20:04 +0200

    generate rtc token

---
+++ index.js
@@ -0,0 +1,143 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { RtcTokenBuilder, RtcRole, RtmTokenBuilder, RtmRole } = require('agora-access-token');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8080;
+const APP_ID = process.env.APP_ID;
+const APP_CERTIFICATE = process.env.APP_CERTIFICATE;
+
+const nocache = (_, resp, next) => {
+    resp.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    resp.header('Expires', '-1');
+    resp.header('Pragma', 'no-cache');
+    next();
+}
+
+const ping = (req, resp) => {
+    resp.send({ message: 'pong' });
+}
+
+const generateRTCToken = (req, resp) => {
+    // set response header
+    resp.header('Access-Control-Allow-Origin', '*');
+    // get channel name
+    const channelName = req.params.channel;
+    if (!channelName) {
+        return resp.status(400).json({ 'error': 'channel is required' });
+    }
+    // get uid
+    let uid = req.params.uid;
+    if (!uid || uid === '') {
+        return resp.status(400).json({ 'error': 'uid is required' });
+    }
+    // get role
+    let role;
+    if (req.params.role === 'publisher') {
+        role = RtcRole.PUBLISHER;
+    } else if (req.params.role === 'audience') {
+        role = RtcRole.SUBSCRIBER
+    } else {
+        return resp.status(400).json({ 'error': 'role is incorrect' });
+    }
+    // get the expire time
+    let expireTime = req.query.expiry;
+    if (!expireTime || expireTime === '') {
+        expireTime = 3600;
+    } else {
+        expireTime = parseInt(expireTime, 10);
+    }
+    // calculate privilege expire time
+    const currentTime = Math.floor(Date.now() / 1000);
+    const privilegeExpireTime = currentTime + expireTime;
+    // build the token
+    let token;
+    if (req.params.tokentype === 'userAccount') {
+        token = RtcTokenBuilder.buildTokenWithAccount(APP_ID, APP_CERTIFICATE, channelName, uid, role, privilegeExpireTime);
+    } else if (req.params.tokentype === 'uid') {
+        token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, channelName, uid, role, privilegeExpireTime);
+    } else {
+        return resp.status(400).json({ 'error': 'token type is invalid' });
+    }
+    // return the token
+    return resp.json({ 'rtcToken': token });
+}
+
+const generateRTMToken = (req, resp) => {
+    // set response header
+    resp.header('Access-Control-Allow-Origin', '*');
+
+    // get uid
+    let uid = req.params.uid;
+    if (!uid || uid === '') {
+        return resp.status(400).json({ 'error': 'uid is required' });
+    }
+    // get role
+    let role = RtmRole.Rtm_User;
+    // get the expire time
+    let expireTime = req.query.expiry;
+    if (!expireTime || expireTime === '') {
+        expireTime = 3600;
+    } else {
+        expireTime = parseInt(expireTime, 10);
+    }
+    // calculate privilege expire time
+    const currentTime = Math.floor(Date.now() / 1000);
+    const privilegeExpireTime = currentTime + expireTime;
+    // build the token
+    console.log(APP_ID, APP_CERTIFICATE, uid, role, privilegeExpireTime)
+    const token = RtmTokenBuilder.buildToken(APP_ID, APP_CERTIFICATE, uid, role, privilegeExpireTime);
+    // return the token
+    return resp.json({ 'rtmToken': token });
+}
+
+const generateRTEToken = (req, resp) => {
+    // set response header
+    resp.header('Access-Control-Allow-Origin', '*');
+    // get channel name
+    const channelName = req.params.channel;
+    if (!channelName) {
+        return resp.status(400).json({ 'error': 'channel is required' });
+    }
+    // get uid
+    let uid = req.params.uid;
+    if (!uid || uid === '') {
+        return resp.status(400).json({ 'error': 'uid is required' });
+    }
+    // get role
+    let role;
+    if (req.params.role === 'publisher') {
+        role = RtcRole.PUBLISHER;
+    } else if (req.params.role === 'audience') {
+        role = RtcRole.SUBSCRIBER
+    } else {
+        return resp.status(400).json({ 'error': 'role is incorrect' });
+    }
+    // get the expire time
+    let expireTime = req.query.expiry;
+    if (!expireTime || expireTime === '') {
+        expireTime = 3600;
+    } else {
+        expireTime = parseInt(expireTime, 10);
+    }
+    // calculate privilege expire time
+    const currentTime = Math.floor(Date.now() / 1000);
+    const privilegeExpireTime = currentTime + expireTime;
+    // build the token
+    const rtcToken = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, channelName, uid, role, privilegeExpireTime);
+    const rtmToken = RtmTokenBuilder.buildToken(APP_ID, APP_CERTIFICATE, uid, role, privilegeExpireTime);
+    // return the token
+    return resp.json({ 'rtcToken': rtcToken, 'rtmToken': rtmToken });
+}
+
+app.options('*', cors());
+app.get('/ping', nocache, ping)
+app.get('/rtc/:channel/:role/:tokentype/:uid', nocache, generateRTCToken);
+app.get('/rtm/:uid/', nocache, generateRTMToken);
+app.get('/rte/:channel/:role/:tokentype/:uid', nocache, generateRTEToken);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
